649b8006af87ed14c09905497e6a5596ec72b733
John Ao <John-Ao@users.noreply.github.com>
2019-08-08 11:33:06 +0800

    replace TAB to 4 spaces

--- sources_1/imports/exp6/DataMemory.v
+++ sources_1/imports/exp6/DataMemory.v
@@ -1,20 +1,20 @@
 module DataMemory(reset, clk, clk_count, Address, Write_data, Read_data, MemRead, MemWrite);
-	input reset, clk;
-	input [31:0] clk_count, Address, Write_data;
-	input MemRead, MemWrite;
-	output reg [31:0] Read_data;
-	
-	parameter RAM_SIZE = 512;
-	parameter RAM_SIZE_BIT = 9;
-	parameter PERI_SIZE = 512;
-	parameter PERI_SIZE_BIT = 9;
-	
-	reg [31:0] RAM_data[RAM_SIZE - 1: 0];
-	reg [31:0] PERI_data[PERI_SIZE - 1: 0];
-	always @(posedge clk)  // if 0x4xxx xxx, use PERI_data
-	   Read_data <= MemRead? (Address[30]?PERI_data[Address[PERI_SIZE_BIT + 1:2]]:RAM_data[Address[RAM_SIZE_BIT + 1:2]]): 32'h00000000;
-	
-	integer i;
+    input reset, clk;
+    input [31:0] clk_count, Address, Write_data;
+    input MemRead, MemWrite;
+    output reg [31:0] Read_data;
+    
+    parameter RAM_SIZE = 512;
+    parameter RAM_SIZE_BIT = 9;
+    parameter PERI_SIZE = 512;
+    parameter PERI_SIZE_BIT = 9;
+    
+    reg [31:0] RAM_data[RAM_SIZE - 1: 0];
+    reg [31:0] PERI_data[PERI_SIZE - 1: 0];
+    always @(posedge clk)  // if 0x4xxx xxx, use PERI_data
+       Read_data <= MemRead? (Address[30]?PERI_data[Address[PERI_SIZE_BIT + 1:2]]:RAM_data[Address[RAM_SIZE_BIT + 1:2]]): 32'h00000000;
+    
+    integer i;
     always @(posedge reset or posedge clk) begin
         if (reset) begin
             for (i = 0; i < RAM_SIZE; i = i + 1)
--- sources_1/imports/exp6/InstructionMemory.v
+++ sources_1/imports/exp6/InstructionMemory.v
@@ -1,11 +1,11 @@
 
 module InstructionMemory(Address, Instruction);
-	input [31:0] Address;
-	output reg [31:0] Instruction;
-	
-	always @(*)      //use negedge to make sure that PC is calculated
-		case (Address[9:2])
-			8'd0:	Instruction<=32'b001000_00000_00100_00000_00000_000011;
+    input [31:0] Address;
+    output reg [31:0] Instruction;
+    
+    always @(*)      //use negedge to make sure that PC is calculated
+        case (Address[9:2])
+            8'd0:	Instruction<=32'b001000_00000_00100_00000_00000_000011;
             8'd1:	Instruction<=32'b000011_00000_00000_00000_00000_000011;
             8'd2:	Instruction<=32'b000100_00000_00000_11111_11111_111111;
             8'd3:	Instruction<=32'b001000_11101_11101_11111_11111_111000;
@@ -23,7 +23,7 @@ module InstructionMemory(Address, Instruction);
             8'd15:	Instruction<=32'b001000_11101_11101_00000_00000_001000;
             8'd16:	Instruction<=32'b000000_00100_00010_00010_00000_100000;
             8'd17:	Instruction<=32'b000000_11111_00000_00000_00000_001000;
-			default: Instruction <= 32'h00000000;
-		endcase
-		
+            default: Instruction <= 32'h00000000;
+        endcase
+        
 endmodule
