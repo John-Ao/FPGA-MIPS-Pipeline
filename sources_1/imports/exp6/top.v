1df54b9932e1ecb858231a71908a60324162c9c0
John Ao <John-Ao@users.noreply.github.com>
2019-08-13 12:25:16 +0800

    Update top.v

--- sources_1/imports/exp6/top.v
+++ sources_1/imports/exp6/top.v
@@ -18,7 +18,7 @@ module top(clk,rst,leds,digits,digit_led_1,digit_led_2,Rx,Tx);
 //            divider<=divider+27'd2;
             
     CPU cpu(rst,clk,Rx,Tx);
-    assign num=top.cpu.if_pc;//data_memory1.PERI_data[4];
+    assign num=top.cpu.data_memory1.PERI_data[4];
     assign leds=top.cpu.data_memory1.PERI_data[3][7:0];
     display d1(clk,num[15:0],digits[3:0],digit_led_1);
     display d2(clk,num[31:16],digits[7:4],digit_led_2);
